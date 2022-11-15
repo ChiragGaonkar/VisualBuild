54783b50b20f33c0c32f2ccf52ec7be950b3a057
Chirag Gaonkar <chiraggaonkar80@gmail.com>
2022-11-15 23:04:51 +0530

    Setting Up Files

---
+++ lib/screens/starter_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class StarterScreen extends StatelessWidget {
+  const StarterScreen({super.key});
+  static const routeName = '/starter';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+      child: Text("Hello World"),
+    ));
+  }
+}
